5648303bbf03ff5e92c224e6108ccd60cbc592aa
Dmitry_Romankov <Dmitry_Romankov@epam.com>
2019-10-24 18:51:55 +0300

    feat: added loader component

---
+++ quickstart/src/components/loader/index.tsx
@@ -0,0 +1,10 @@
+import React, {memo} from 'react';
+import './styles.css';
+
+export const Loader = memo(function Loader() {
+  return (
+    <div className="loader">
+      <div className="loader__component" ></div>
+    </div>
+  );
+});
--- quickstart/src/components/search/index.tsx
+++ quickstart/src/components/search/index.tsx
@@ -1,24 +1,28 @@
-import React, { useState, memo, useCallback, ChangeEvent, FormEvent } from 'react';
-import { Input } from 'components/input';
-import { WellFile } from 'components/well-file';
-import { SearchResult, SearchResultItem } from 'models';
+import React, {useState, memo, useCallback, ChangeEvent, FormEvent, MouseEvent} from 'react';
+import {Input} from 'components/input';
+import {WellFile} from 'components/well-file';
+import {Loader} from 'components/loader';
+import {SearchResult, SearchResultItem} from 'models';
 import './styles.css';
 
 interface Props {
   onVizualize: () => void;
 }
 
-export const Search = memo(function Search({ onVizualize }: Props) {
+export const Search = memo(function Search({onVizualize}: Props) {
   const [wellFiles, setWellFillesVisible] = useState<SearchResultItem[]>([]);
   const [search, setSearch] = useState('A05-01');
+  const [isLoaderShown, showLoader] = useState(false);
 
   const showWellFiles = useCallback(
-    (event: FormEvent) => {
+    (event: FormEvent | MouseEvent) => {
       event.preventDefault();
+      showLoader(true);
       fetch(`/find?wellname=${search}`)
         .then(response => response.json())
         .then((data: SearchResult) => {
           setWellFillesVisible(data['work-product-component/WellborePath'] || []);
+          showLoader(false);
         });
     },
     [search]
@@ -38,15 +42,18 @@ export const Search = memo(function Search({ onVizualize }: Props) {
           onChange={handleSearchChange}
           value={search}
         />
+        <Input className="search__submit" type="submit" value="Search" onClick={showWellFiles} />
       </form>
-      {wellFiles.map(well => (
-        <WellFile
-          key={well.filename}
-          fileName={well.filename}
-          fileType={well.srn}
-          onVizualize={onVizualize}
-        />
-      ))}
+      <div className="search__well-area">
+        {isLoaderShown ? <Loader /> : wellFiles.map(well => (
+          <WellFile
+            key={well.filename}
+            fileName={well.filename}
+            fileType={well.srn}
+            onVizualize={onVizualize}
+          />
+        ))}
+      </div>
     </div>
   );
 });
