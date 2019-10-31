69e23167462dd7cbe54cc29c14ae0245eefdbe74
Valiantsin <valiantsin.haradkou@icloud.com>
2019-10-31 14:29:22 +0300

    feat: settings for microservices

--- quickstart/server.js
+++ quickstart/server.js
@@ -1,15 +1,44 @@
 const express = require('express');
 const path = require('path');
-const proxy = require('express-http-proxy');
+const proxy = require('http-proxy-middleware');
 
 const app = express();
 
 // Serve the static files from the React app
 app.use(express.static(path.join(__dirname, 'build')));
 
-const proxyPort = process.env.SERVER_PORT || '8080';
-const proxyHost = process.env.SERVER_HOST || 'server';
-app.use('/api', proxy(`http://${proxyHost}:${proxyPort}`));
+const findProxyPort = process.env.FIND_SERVER_PORT || '8080';
+const findProxyHost = process.env.FIND_SERVER_HOST || 'server';
+app.use(
+  proxy('/api/find', {
+    target: `http://${findProxyHost}:${findProxyPort}`,
+    pathRewrite: {
+      '^/api': '/', // rewrite path
+    },
+  })
+);
+
+const fetchProxyPort = process.env.FETCH_SERVER_PORT || '8080';
+const fetchProxyHost = process.env.FETCH_SERVER_HOST || 'server';
+app.use(
+  proxy('/api/fetch', {
+    target: `http://${fetchProxyHost}:${fetchProxyPort}`,
+    pathRewrite: {
+      '^/api': '/', // rewrite path
+    },
+  })
+);
+
+const authProxyPort = process.env.AUTH_SERVER_PORT || '8080';
+const authProxyHost = process.env.AUTH_SERVER_HOST || 'server';
+app.use(
+  proxy('/api/auth', {
+    target: `http://${authProxyHost}:${authProxyPort}`,
+    pathRewrite: {
+      '^/api': '/', // rewrite path
+    },
+  })
+);
 
 // Handles any requests that don't match the ones above
 app.get('*', (req, res) => {
--- quickstart/src/setupProxy.js
+++ quickstart/src/setupProxy.js
@@ -1,7 +1,23 @@
 const proxy = require('http-proxy-middleware');
 module.exports = function(app) {
   app.use(
-    proxy('/api/*', {
+    proxy('/api/find', {
+      target: 'http://localhost:8080',
+      pathRewrite: {
+        '^/api': '/', // rewrite path
+      },
+    })
+  );
+  app.use(
+    proxy('/api/fetch', {
+      target: 'http://localhost:8080',
+      pathRewrite: {
+        '^/api': '/', // rewrite path
+      },
+    })
+  );
+  app.use(
+    proxy('/api/auth', {
       target: 'http://localhost:8080',
       pathRewrite: {
         '^/api': '/', // rewrite path
